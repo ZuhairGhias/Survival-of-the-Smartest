d9a3cae8c83e4e9b786027b706899869b8ebb063
AMastryukov <Alexandermastryukov@gmail.com>
2018-04-21 18:36:35 -0400

    Remove ProCore from remote repo
    Add ProCore files to .gitignore
    Add non-functional door for testing

---
+++ Survival of the Smartest/Assets/Scripts/Doors/DoubleDoorBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleDoorBehavior : MonoBehaviour {
+
+    public GameObject leftDoorPivot;
+    public GameObject rightDoorPivot;
+
+    public void OpenDoors()
+    {
+
+    }
+}
