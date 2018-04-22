a82622878a940971b621a0c260694ee4ecafd2e9
AMastryukov <Alexandermastryukov@gmail.com>
2018-04-22 19:25:00 -0400

    Add door triggers
    Add door trigger for opening and closing
    Add script to handle opening and closing

---
+++ Survival of the Smartest/Assets/Scripts/Doors/DoorTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour {
+
+    public DoubleDoorBehavior door; // door to work with
+    public bool openDoor = false; // indicates whether to open the door upon entering trigger (true opens, false closes)
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // if the collider is the player object, handle behavior
+        if (other.gameObject.GetComponent<CharacterController>() != null)
+        {
+            if (openDoor)
+            {
+                door.OpenDoors();
+            } else
+            {
+                // disable the door
+                door.CloseDoors();
+                door.doorEnabled = false;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // if the collider is the player object, handle behavior
+        if (other.gameObject.GetComponent<CharacterController>() != null)
+        {
+            if (openDoor)
+            {
+                door.CloseDoors();
+            }
+        }
+    }
+}
