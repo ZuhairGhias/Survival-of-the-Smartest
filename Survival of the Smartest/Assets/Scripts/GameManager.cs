c518e02f54114ce44000eb99ceec15609bb0ebb0
Zuhair <zuhair.ghias@hotmail.com>
2018-04-21 22:17:19 -0400

    Start work on dynamic hallways

---
+++ Survival of the Smartest/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private GameObject currentHall;
+    public GameObject hallPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GenerateNextHall(GameObject currentHall) {
+        this.currentHall = currentHall;
+        GameObject rightHall = Instantiate(hallPrefab, currentHall.transform.Find("HallRight").position, currentHall.transform.Find("HallRight").rotation);
+
+
+    }
+}
