8655c714e7d9b1f6f8fde8afd881902a37f6c2cd
Zuhair <zuhair.ghias@hotmail.com>
2018-04-22 19:23:03 -0400

    Added dimming lights

--- Survival of the Smartest/Assets/Scripts/Hallway.cs
+++ Survival of the Smartest/Assets/Scripts/Hallway.cs
@@ -11,9 +11,13 @@ public class Hallway : MonoBehaviour
     public TextMeshProUGUI leftAnswer;
     public TextMeshProUGUI question;
 
+    public float minLightIntensity = 0f;
+    public float lightDimmingRate = 0.05f;
+
     private bool isGoodHall = true; // Tracks whether this hall is the correct or wrong hall
     private bool bias = true; // bias == true <=> right hall is good
     private int spookiness; // how spooky is the hallway
+    
 
     public void SetQuestion(Question question, bool bias)
     {
@@ -51,7 +55,23 @@ public class Hallway : MonoBehaviour
     {
         this.spookiness = spookiness;
 
+        StartCoroutine(DimLights(spookiness));
         // put spooky logic here
 
+    }
+
+    private IEnumerator DimLights(float spookiness)
+    {
+        Light[] lights = GetComponentsInChildren<Light>();
+        
+        float targetIntensity = 1 - (spookiness / 30) * (1 - minLightIntensity);
+        print(targetIntensity);
+        while (lights[0].intensity > targetIntensity) {
+            for (int i = 0; i < lights.Length; i++) {
+                lights[i].intensity = lights[i].intensity - lightDimmingRate;
+            }
+            yield return null;
+        }
+        yield return null;
     }
 }
