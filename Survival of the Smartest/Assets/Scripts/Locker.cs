47893105f085ab2ae8dbb683316cdba1bd85e85d
Zuhair <zuhair.ghias@hotmail.com>
2018-04-23 13:03:59 -0400

    Add locker scare

---
+++ Survival of the Smartest/Assets/Scripts/Locker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Locker : MonoBehaviour {
+
+    public Transform door;
+
+    private bool isOpen = false;
+    public float openRate = 1;
+    public AudioSource doorSound;
+
+    private Quaternion closedRotation;
+    private Quaternion openRotation;
+
+    // Use this for initialization
+    void Start () {
+        openRotation = Quaternion.Euler(0, -90, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isOpen) {
+            door.localRotation = Quaternion.Slerp(door.localRotation, openRotation, Time.deltaTime*openRate);
+            
+        }
+	}
+
+    public void Open() {
+        if (!isOpen) {
+
+            doorSound.Play();
+        }
+        this.isOpen = true;
+    }
+}
---
+++ Survival of the Smartest/Assets/Scripts/LockerScare.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockerScare : MonoBehaviour {
+
+    private bool scareEnabled = false;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider col) {
+        if (col.tag == "Player" && !scareEnabled) {
+            Locker[] lockers = GetComponent<Collider>().transform.root.GetComponentsInChildren<Locker>();
+            print(col.transform.root.name);
+            print(lockers.Length);
+            lockers[Random.Range(0, lockers.Length)].Open();
+            scareEnabled = true;
+        }
+    }
+}
