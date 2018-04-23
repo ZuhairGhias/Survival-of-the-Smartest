57f30bac00624f5bcad74e2ebd34c124c36bfdfe
AMastryukov <Alexandermastryukov@gmail.com>
2018-04-22 22:40:30 -0400

    Add footstep sounds
    Add footstep behavior to movement script

--- Survival of the Smartest/Assets/Scripts/FPS Controller/FPSMovementControl.cs
+++ Survival of the Smartest/Assets/Scripts/FPS Controller/FPSMovementControl.cs
@@ -5,16 +5,38 @@ using UnityEngine;
 public class FPSMovementControl : MonoBehaviour {
 
     CharacterController characterController;
-    public float walkSpeed;
+    public float walkSpeed = 3f;
+    public float strideLength = 1.5f;
+
+    // footstep sounds
+    public AudioSource footstepSource;
+    public AudioClip[] footstepSounds;
+
+    private Vector3 lastPosition;
 
     void Awake()
     {
         characterController = GetComponent<CharacterController>();
+        lastPosition = transform.position;
     }
 
     void Update()
     {
         MovePlayer();
+
+        // play footstep sound
+        if (characterController.isGrounded == true && 
+            GetComponent<AudioSource>().isPlaying == false &&
+            (Vector3.Distance(transform.position, lastPosition) > strideLength))
+        {
+            // select a random footstep sound and play it
+            footstepSource.clip = footstepSounds[Random.Range(0, footstepSounds.Length)];
+            footstepSource.pitch = Random.Range(0.9f, 1.1f);
+            footstepSource.Play();
+
+            // set the last transform position
+            lastPosition = transform.position;
+        }
     }
 
     void MovePlayer()
