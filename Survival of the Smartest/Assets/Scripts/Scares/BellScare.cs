f2a56e11c622916a13cfd6e49ebcac8e9f93f9ad
AMastryukov <Alexandermastryukov@gmail.com>
2018-04-23 15:59:28 -0400

    Add Bell Scare

---
+++ Survival of the Smartest/Assets/Scripts/Scares/BellScare.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BellScare : MonoBehaviour {
+
+    public AudioSource bell;
+
+    void OnTriggerEnter(Collider other)
+    {
+        print("BELL TRIGGERED");
+        bell.Play();
+        Destroy(this.gameObject);
+    }
+}
--- Survival of the Smartest/Assets/Scripts/Scares/DoorScare.cs
+++ Survival of the Smartest/Assets/Scripts/Scares/DoorScare.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorScare : MonoBehaviour {
-
+    
     public AudioSource knockingSound;
     public Collider trigger1;
     public Transform spawnLocation;
